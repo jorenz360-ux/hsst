9355c48a625fe3c4aafc330e0e8cb23a20599c0d
Your Name <youremail@example.com>
2026-03-14 13:33:05 +0800

    Fix users migration remove duplicate column usernamea in another migration

--- database/migrations/2026_01_11_084204_update_users_table.php
+++ database/migrations/2026_01_11_084204_update_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration {
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeign(['alumni_id']);
-            $table->dropColumn(['username', 'role', 'alumni_id']);
         });
     }
 };
