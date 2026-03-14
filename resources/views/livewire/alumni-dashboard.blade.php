207b74718a939d7eebb64005f2f74c3922694cb0
Your Name <youremail@example.com>
2026-03-14 17:39:31 +0800

    Fix user profile exception

--- resources/views/livewire/alumni-dashboard.blade.php
+++ resources/views/livewire/alumni-dashboard.blade.php
@@ -155,7 +155,7 @@
                         </a>
                     @else
                         @if(auth()->user()->alumni_id === null)
-                            <a href="{{ route('profile') }}"
+                            <a href="{{ route('settings/profile') }}"
                             class="inline-flex justify-center rounded-lg border border-zinc-200 px-3 py-1.5 text-sm font-medium
                                     text-zinc-900 hover:bg-zinc-50 dark:border-zinc-700 dark:text-zinc-100 dark:hover:bg-zinc-800/40">
                                 Complete Profile
