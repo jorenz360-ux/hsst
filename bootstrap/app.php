670a3b57792512817b95e115aad6722f96451be2
Your Name <youremail@example.com>
2026-03-22 11:36:20 +0800

    fix proxy issue in uploading prof of payment in production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
@@ -14,20 +15,25 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-          $middleware->alias([
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+
+        $middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
-         $middleware->validateCsrfTokens(except: [
+
+        $middleware->validateCsrfTokens(except: [
             'webhook/paymongo',
         ]);
     })
-//     ->withMiddleware(function (Middleware $middleware) {
-//     $middleware->trustProxies(at: '*');
-// })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-    
-
