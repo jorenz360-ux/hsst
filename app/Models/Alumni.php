1884a24af698973a9f55f0d49eefc31f06a3f33a
Your Name <youremail@example.com>
2026-04-19 09:08:15 +0800

    fix full name format

--- app/Models/Alumni.php
+++ app/Models/Alumni.php
@@ -2,11 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Alumni extends Model
 {
     use HasFactory;
+
+    protected function fname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? ucwords(strtolower($value), " \t\r\n\f\v-.'") : $value,
+        );
+    }
+
+    protected function lname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? ucwords(strtolower($value), " \t\r\n\f\v-.'") : $value,
+        );
+    }
+
+    protected function mname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? ucwords(strtolower($value), " \t\r\n\f\v-.'") : $value,
+        );
+    }
 protected $table = 'alumni';
 
 protected $fillable = [
